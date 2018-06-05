4a13ce6520a0bcd00b8c30f161867c6872c26b98
MerlinTheElf <joakim.selvik@live.no>
2018-06-05 11:28:29 +0200

    Ny test

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -5,5 +5,6 @@ public class GUI {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("Hello World");
+        System.out.println("Ny test");
     }
 }
